9a5c6aca5cdf5c3b93d62413146bb30bc180bdc1
mdawaina <mohameddawaina@gmail.com>
2018-01-08 20:09:26 +0300

    asqwe324dr3423
    der456
    tggf4563453

--- www/js/my-app.js
+++ www/js/my-app.js
@@ -156,11 +156,11 @@ myApp.onPageInit('index', function (page) {
       var deviceType = (navigator.userAgent.match(/iPad/i)) == "iPad" ? "iPad" : (navigator.userAgent.match(/iPhone/i)) == "iPhone" ? "iPhone" : (navigator.userAgent.match(/Android/i)) == "Android" ? "Android" : (navigator.userAgent.match(/BlackBerry/i)) == "BlackBerry" ? "BlackBerry" : "null";
       var sharelink = '';
       if (deviceType == "iPad" || deviceType =="iPhone"){
-          sharelink = 'https://play.google.com/store/apps/details?id=com.phonegap.safir';
+          sharelink = 'https://itunes.apple.com/sa/app/safer-app/id1323553481?mt=8';
       }
       else if (deviceType == "Android") {
        
-              sharelink = '';          
+          sharelink = 'https://play.google.com/store/apps/details?id=com.phonegap.safir';          
        }        
        var options = {
            message: 'حمل تطبيق سافر واستمتع بتجربة سفر مميزة!', // not supported on some apps (Facebook, Instagram)
@@ -888,7 +888,7 @@ $fileURI = $$('#imgArea').attr('src');
         options.fileKey = "uploadfile";
         options.fileName = 'tmp_name'; // fileURI.substr(fileURI.lastIndexOf('/') + 1);
         options.mimeType = "image/jpeg";
-       // options.params = { captain_id: captain_id}; // if we need to send parameters to the server request
+        options.params = {}; // { captain_id: captain_id}; // if we need to send parameters to the server request
         var ft = new FileTransfer();
 
         var win = function (r) {
@@ -952,13 +952,13 @@ $fileURI = $$('#imgArea').attr('src');
 
 });
 
- myApp.onPageInit('trips', function (page) {
+ myApp.onPageInit('trips1', function (page) {
     $$("#tripsul").html(page.context['trip_context']);   
 });
  
 
 
-myApp.onPageInit('trips1', function (page) {
+myApp.onPageInit('trips', function (page) {
     
     
          $$.ajax({
@@ -983,6 +983,14 @@ myApp.onPageInit('trips1', function (page) {
                '</a>'+
              '</li>');
               });
+
+                 mainView.router.loadPage({
+                        url: 'trips.html',
+                        ignoreCache: true,
+                        reload: false,
+                        context: { trip_context: trip_context }
+                    }); 
+
             },
             error: function (data) {
                 console.log('An error occurred.');
@@ -1079,257 +1087,6 @@ myApp.onPageInit('trip_detail', function (page) {
 
 
 
-
-myApp.onPageInit('captain_profile', function (page) {
-
-    myApp.showIndicator();
-
-   
-    $$.ajax
-    ({
-        type: 'POST',
-        url: captain + 'captain_profile/', // + captain_id,
-        data: JSON.stringify({ captain_id: window.localStorage.getItem("captain_id")}),        
-        success: function (resp) {
-            myApp.hideIndicator();
-            console.log(resp.captain_data.name);
-            $$("#name").val(resp.captain_data.name);
-            $$("#email").val(resp.captain_data.email);
-            $$("#password").val(resp.captain_data.password);
-            $$("#mobile").val(resp.captain_data.mobile);
-            $$("#brand").val(resp.captain_data.brand);
-            $$("#model").val(resp.captain_data.model);
-            $$("#year").val(resp.captain_data.year);
-            
-            $$("#imgArea").attr("src", carimage + resp.captain_data.image + '#' + new Date().getTime());
-//            $$("#imgArea").attr("src", carimage + resp.captain_data.image + '#'  + new Date().getTime());
-        },
-        error: function (data) {
-            myApp.hideIndicator();
-            console.log('An error occurred.');
-            console.log(data);
-        }           
-        , dataType: 'json'
-    });
-
-
-
-    $$(document).on('click', '.edit-profile', function () {
-
-        //event.preventDefault();
-        $$('#name').removeAttr("disabled");
-        $$('#email').removeAttr("disabled");
-        $$('#password').removeAttr("disabled");
-        $$('#mobile').removeAttr("disabled");
-        $$('#brand').removeAttr("disabled");
-        $$('#model').removeAttr("disabled");
-        $$('#year').removeAttr("disabled");
-
-       
-        
-        $$(".update-butonn-to-be-here").html('<p><a href="#" class="button button-fill color-green" id="update_profile">تحديث</a></p>');
-    });
-
-    $$('#imgArea').on('click', function () {
-        console.log("hello");
-
-        var options = {
-            quality: 50,
-            destinationType: Camera.DestinationType.FILE_URI,
-            encodingType: Camera.EncodingType.JPEG,
-            mediaType: Camera.MediaType.PICTURE,
-            targetWidth: 600,
-            targetHeight: 400,
-            correctOrientation: true,
-            // sourceType: navigator.camera.PictureSourceType.PHOTOLIBRARY // not originally included
-            sourceType: Camera.PictureSourceType.CAMERA //Camera.PictureSourceType.PHOTOLIBRARY,
-        }
-        navigator.camera.getPicture(onSuccess, onFail, options);
-
-        function onFail(message) {
-            alert('Failed because: ' + message);
-        }
-
-        function clearCache() {
-            navigator.camera.cleanup();
-        }
-
-        var retries = 0;
-        function onSuccess(fileURI) {
-
-            $$("#imgArea").attr("src", fileURI);
-
-            var win = function (r) {
-                clearCache();
-                retries = 0;
-                //alert('Done!');
-            }
-
-            var fail = function (error) {
-                if (retries == 0) {
-                    retries++
-                    setTimeout(function () {
-                        onSuccess(fileURI)
-                    }, 1000)
-                } else {
-                    retries = 0;
-                    clearCache();
-                    alert('Ups. Something wrong happens!');
-                }
-            }
-
-            var options = new FileUploadOptions();
-            options.fileKey = "uploadfile";
-            options.fileName = "tmp_name"; //  fileURI.substr(fileURI.lastIndexOf('/') + 1);
-            options.mimeType = "image/jpeg";
-            options.params = {}; // if we need to send parameters to the server request
-            var ft = new FileTransfer();
-            ft.upload(fileURI, encodeURI(upload_image), win, fail, options);
-
-        }
-
-
-
-    });
-
-    function update_car_image(captain_id) {
-        var fileURI = $$('#imgArea').attr('src');
-
-        var options = new FileUploadOptions();
-        options.fileKey = "uploadfile";
-        options.fileName = 'tmp_name'; // fileURI.substr(fileURI.lastIndexOf('/') + 1);
-        options.mimeType = "image/jpeg";
-        options.params = { captain_id: captain_id }; // if we need to send parameters to the server request
-        var ft = new FileTransfer();
-
-        var win = function (r) {
-             mainView.router.loadPage({
-                url: 'captain_profile.html',
-                ignoreCache: true,
-                reload: true
-            });  
-
-            console.log('ppp'+captain_id);
-        }
-
-        var fail = function (error) {
-
-            console.log('Ups. Something wrong happens!' + error);
-        }
-
-        ft.upload(fileURI, encodeURI(upload_image + '/' + captain_id), win, fail, options);
-        console.log("khlas done!");
-    }
-
-    //do the update
-
-    $$(document).on('click', '#update_profile', function () {
-
-        myApp.showIndicator();
-        $captain_id = window.localStorage.getItem("captain_id");
-        $name = $$('#name').val();
-        $email = $$('#email').val();
-        $password = $$('#password').val();
-        $mobile = $$('#mobile').val();
-        $brand = $$('#brand').val();
-        $model = $$('#model').val();
-        $year = $$('#year').val();
-
-        var isvalid = true;
-
-        if ($name === '') {
-            myApp.alert('', "الاسم مطلوب");
-            isvalid = false;
-            myApp.hideIndicator();
-        }
-
-        if ($email === '') {
-            myApp.alert('', "البريد الالكتروني مطلوب");
-            isvalid = false;
-            myApp.hideIndicator();
-        }
-
-        if ($password === '') {
-            myApp.alert('', "كلمة المرور مطلوبة");
-            isvalid = false;
-            myApp.hideIndicator();
-        }
-
-        if ($mobile === '') {
-            myApp.alert('', "رقم الجوال مطلوب");
-            isvalid = false;
-            myApp.hideIndicator();
-        }
-
-        if ($brand === '') {
-            myApp.alert('', "العلامة التجارية للسيارة مطلوبة");
-            isvalid = false;
-            myApp.hideIndicator();
-        }
-
-        if ($model === '') {
-            myApp.alert('', "موديل السيارة مطلوب");
-            isvalid = false;
-            myApp.hideIndicator();
-        }
-
-        if ($year === '') {
-            myApp.alert('', "سنة الصنع مطلوبة");
-            isvalid = false;
-            myApp.hideIndicator();
-        }
-
-       /*  if ($fileURI === '') {
-            myApp.alert('', "ارفق صورة للسيارة الخاصة بك");
-            isvalid = false;
-            myApp.hideIndicator();
-        } */
-
-      
-       if(isvalid){
-        $$.ajax({
-            type: 'POST',
-            url: captain + 'update_captain/',
-            
-            data: JSON.stringify({
-                id: $captain_id,
-                name: $name,
-                email: $email,
-                password: $password,
-                mobile: $mobile,
-                brand: $brand,
-                model: $model,
-                year: $year
-
-            }),
-            success: function (data) {
-                myApp.hideIndicator();
-                if(data.code == 0){
-
-                    update_car_image($captain_id);
-                   
-                }
-                console.log(data.code);                
-            },
-            error: function (data) {
-                myApp.hideIndicator();
-                console.log('An error occurred.');
-                console.log(data);
-            }//,
-            
-            , dataType: 'json'
-        });
-    }
-   // e.preventDefault();
-
-
-
-
-    });
-
-
-
-});
 
 myApp.onPageInit('rating', function (page) {
 
